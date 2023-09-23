9483cecd9c692a940964764567029bd351cabc23
Wei Ting Wu <weiting@projectmars.info>
2023-09-23 13:42:00 +0800

    feat(server): setup express server and mongodb

--- src/server.js
+++ src/server.js
@@ -1,15 +1,49 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require('mongoose')
+const path = require('path')
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+require('dotenv').config()
+
+const uri = process.env.MONGO_URI
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}
+
+// 連接到 MongoDB
+mongoose
+	.connect(uri, options)
+	.then(() => {
+		console.log('MongoDB is connected')
+	})
+	.catch((err) => {
+		console.log(err)
+	})
+
+const app = express()
+
+const PORT = 3000
+
+app.use(express.json())
 
 app.get('*', (req, res) => {
-	res.status(404).send('Page Not Found')
+	res.send('Default route is working')
 })
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+app.use((err, req, res, next) => {
+	const status = err.status || 500
+	if (status === 500) {
+		console.log('The server errored when processing a request')
+		console.log(err)
+	}
+
+	res.status(status)
+	res.send({
+		status: status,
+		message: err.message,
+	})
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`)
 })
