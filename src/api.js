cc8194a7f16d8b3721cc0d4317e6995e1c75b4c1
Wei Ting Wu <weiting@projectmars.info>
2023-09-23 16:39:43 +0800

    feat(api-docs): add api docs, fix post session input

--- src/api.js
+++ src/api.js
@@ -5,6 +5,41 @@ const openAIChat = require('./utils/openai')
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * definitions:
+ *   Session:
+ *     properties:
+ *       user:
+ *         type: string
+ *       title:
+ *         type: string
+ *       createdAt:
+ *         type: string
+ *         format: date-time
+ *       messages:
+ *         type: array
+ *         items:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /session:
+ *   get:
+ *     description: Returns sessions with selected fields
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: successful operation
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/Session'
+ *       500:
+ *         description: Internal Server Error
+ */
 router.get('/session', async (req, res) => {
 	try {
 		// 只回傳 user, title, 和 createdAt 的部分
@@ -14,6 +49,30 @@ router.get('/session', async (req, res) => {
 		res.status(500).json({ error: 'Internal Server Error' })
 	}
 })
+
+/**
+ * @swagger
+ * /session/user/{userId}:
+ *   get:
+ *     description: Get sessions based on a specific user ID
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: userId
+ *         description: User's ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successful operation. Returns sessions associated with the provided user ID.
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/Session'
+ *       500:
+ *         description: Internal Server Error
+ */
 
 router.get('/session/user/:userId', async (req, res) => {
 	try {
@@ -27,6 +86,27 @@ router.get('/session/user/:userId', async (req, res) => {
 	}
 })
 
+/**
+ * @swagger
+ * /session/{sessionId}:
+ *   get:
+ *     description: Get a specific session by its ID
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: sessionId
+ *         description: Session's ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successful operation. Returns the session associated with the provided session ID.
+ *         schema:
+ *           $ref: '#/definitions/Session'
+ *       500:
+ *         description: Internal Server Error
+ */
 router.get('/session/:sessionId', async (req, res) => {
 	try {
 		const session = await Session.findById(req.params.sessionId)
@@ -36,6 +116,31 @@ router.get('/session/:sessionId', async (req, res) => {
 	}
 })
 
+/**
+ * @swagger
+ * /session/{sessionId}:
+ *   delete:
+ *     description: Delete a specific session by its ID
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: sessionId
+ *         description: Session's ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Session successfully deleted
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               default: "Session deleted"
+ *       500:
+ *         description: Internal Server Error
+ */
 router.delete('/session/:sessionId', async (req, res) => {
 	try {
 		await Session.findByIdAndDelete(req.params.sessionId)
@@ -45,21 +150,94 @@ router.delete('/session/:sessionId', async (req, res) => {
 	}
 })
 
-router.post('/session', async (req, res) => {
+/**
+ * @swagger
+ * /session/{userId}:
+ *   post:
+ *     description: Create a new session for a specified user with current date-time title
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: userId
+ *         description: ID of the user for whom the session is to be created
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Session successfully created for the specified user
+ *         schema:
+ *           type: object
+ *           properties:
+ *             _id:
+ *               type: string
+ *             user:
+ *               type: string
+ *             title:
+ *               type: string
+ *             messages:
+ *               type: array
+ *               items:
+ *                 type: string
+ *             createdAt:
+ *               type: string
+ *               format: date-time
+ *       500:
+ *         description: Internal Server Error
+ */
+router.post('/session/:userId', async (req, res) => {
 	try {
+		console.log(req.params.userId)
 		const session = new Session({
-			user: req.body.user,
-			title: req.body.title,
+			user: req.params.userId,
+			title: new Date().toLocaleString(),
 			messages: [],
 			createdAt: new Date(),
 		})
 		await session.save()
 		res.json(session)
 	} catch (error) {
+		console.log(error)
 		res.status(500).json({ error: 'Internal Server Error' })
 	}
 })
 
+/**
+ * @swagger
+ * definitions:
+ *   MessageContent:
+ *     properties:
+ *       content:
+ *         type: string
+ *         description: The message content to be added to the session's messages array
+ *
+ * /session/{sessionId}/messages:
+ *   put:
+ *     description: Add a message to a specific session and get a response message from the system
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: sessionId
+ *         description: Session's ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: body
+ *         description: Message content object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/MessageContent'
+ *     responses:
+ *       200:
+ *         description: Session successfully updated with the new message and the system's response
+ *         schema:
+ *           $ref: '#/definitions/Session'
+ *       500:
+ *         description: Internal Server Error
+ */
 router.put('/session/:sessionId/messages', async (req, res) => {
 	try {
 		const session = await Session.findById(req.params.sessionId)
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
 require('dotenv').config()
 
@@ -23,13 +25,34 @@ mongoose
 		console.log(err)
 	})
 
+// 設定 api docs 的資訊
+const swaggerOptions = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'AIChat Docs',
+			version: '1.0.0',
+			description: 'Api Docs for AIChat',
+		},
+	},
+	// Path to the API docs
+	apis: ['./src/api.js'],
+}
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions)
+
 const app = express()
 
 const PORT = 3333
 
 app.use(express.json())
 app.use(cors())
-app.use('/api', api)
+app.use('/', api)
+app.use(
+	'/api-docs',
+	swaggerUi.serve,
+	swaggerUi.setup(swaggerSpec, { explorer: true })
+)
 
 app.get('*', (req, res) => {
 	res.send('Default route is working')
